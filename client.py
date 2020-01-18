0a0d201717e68b85528e539eac62f74d25c84503
“ernandez” <ernandez.santos@academico.ifpb.edu.br>
2020-01-18 11:10:21 -0300

    Incluindo Documentaçao ao codigo e outras melhorias

--- client.py
+++ client.py
@@ -1,30 +1,63 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" A implementação da aplicação será baseada nos códigos do projeto, Conversão de expressões, desenvolvido na disciplina Programação e Estruturas de Dados do curso de redes de computados do IFPB.
+
+    Versão do Python em que foi testada: 3.6
+
+    Cliente:
+	Após a execução da aplicação cliente, será iniciada uma conexão com o servidor, em seguida será solicitado ao usuário a digitação de uma expressão infixa.
+        exemplo: A + B * C
+	
+    1. O comando “posfixa” solicitará ao servidor a conversão da expressão infixa anteriormente digitada em uma expressão posfixa, mostrando a conversão em tela do cliente.
+        exemplo: A B C * +
+	
+    2. O comando “prefixa” solicitará ao servidor a conversão da expressão infixa anteriormente digitada em uma expressão prefixa, mostrando a conversão em tela do cliente.
+        exemplo: + A * B C
+	
+    3. A opção “exit” fecha a conexão do lado cliente da aplicação.
+"""
 
 import socket
 import sys
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
 if len(sys.argv) > 1:
     HOST = sys.argv[1]
-print("Host:", HOST)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        print("-------------------------------")
-        try:
-           msg = input("Expressão inFixa: ");
-        except: break
-        s.sendall(str.encode(msg))
-        
-        try:
-            formato=input("preFixa ou posFixa or exit: ")
-        except: break
-        s.sendall(str.encode(formato))
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print("Conectado ao Servidor:", HOST)
+        while True:
+            try:
+                print("\nConversor de Expressões Infixa")
+                print("=-="* 10)
+                print("Expressão Infixa \t1")
+                print("Exit             \t2")
+                print("=-="* 10)
+                op = input (">>>> Opção: ")
+                assert op=="1" or op=="2"
+            
+                if (op == "1"):
+                    msg = input("\nDigite a Expressão InFixa: ")            
+                    s.sendall(str.encode(msg))
+                
+                    formato=input("preFixa ou posFixa: ")
+                    s.sendall(str.encode(formato))
+                
+                    data = s.recv(1024)
+                    dados=data.decode()
+                    print("Expressão Modificada: ",dados)
+                elif (op == "2"):
+                    s.close()
+                    break
+            except AssertionError:
+                print('\nOpção inválida. Tente uma opção do menu!')
 
-        data = s.recv(1024)
-        dados=data.decode()
-        print("Expressão Modificada: ",dados)
-
+except KeyboardInterrupt:
+    print(" Aplicação cliente fechada!")
+except:
+    print(" Servidor indisponível no momento. Por favor tente mais tarde.")
--- server.py
+++ server.py
@@ -1,36 +1,66 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+""" A implementação da aplicação será baseada nos códigos do projeto, Conversão de expressões, desenvolvido na disciplina Programação e Estruturas de Dados do curso de redes de computados do IFPB.
+    
+    Versão do Python em que foi testada: 3.6
+        
+    Servidor:
+    Após a execução da aplicação o servidor ficará em listen aguardando solicitações de clientes.
+    
+    1. Após a conexão do cliente, o servidor receberá a (1)Expressão Infixa e o (2)Nome do método para converter (prefixa ou posfixa)
+	
+    2. Caso a solicitação seja “posfixa”, o servidor converterá a expressão infixa recebida em uma expressão posfixa e enviará ao cliente.
+	
+    3. Caso a solicitação seja “prefixa”, o servidor converterá a expressão infixa recebida em uma expressão prefixa e enviará ao cliente.
+	
+    4. Caso a solicitação seja “exit”, o servidor fechará seu lado de conexão com o cliente.
+	
+    O servidor continuará em listen, aguardando novas conexões, até que se encerre a aplicação com Ctrl + z.
+"""
+
 import socket
+import os
+from ClasseExpressao import *
 
 HOST = '0.0.0.0' # Any network interface available
 PORT = 65432 # Port to listen on (non-privileged ports are > 1023)
 
-from ClasseExpressao import *
+try:
+    tcp=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.bind((HOST, PORT))
+    tcp.listen(1)
+    print ("[*] Servidor escutando ...")
+    print("Pid Principal:",os.getpid())
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
     while True:
-        try:
-            conn, addr = s.accept()
-        except: break
-        with conn:
-            print('Connected by', addr)
+        conn, addr = tcp.accept()
+        newpid=os.fork() #clona o processo pai, cria um pid novo para o processo clonado
+        print("Processo filho:", newpid)
+        if(newpid == 0):
+            tcp.close()
+            print("Conectado pelo cliente: ",addr)
             while True:
-                data = conn.recv(1024)
-                expressao=data.decode()
-                print ("Primeiro dado cliente:", expressao)
-                if not data: break
-                exp=Expressao(expressao) # Instanciando um objeto
+                data1 = conn.recv(1024) # Primeiro dado recebido, expressão infixa 
+                dado = data1.decode()
+                if (dado=='exit' or dado==''):
+                    print("Fechada a conexão com o cliente; ",addr)
+                    conn.close()
+                    os._exit(0)
+                else:
+                    exp=Expressao(dado)
                     
-                data = conn.recv(1024)
-                formato=data.decode()
-                print("Segundo dados do cliente:", formato)
-               
-                if(formato=="posFixa"):
-                    data=exp.posFixa()
-                    conn.sendall(data.encode()) #Enviar dado para o cliente
-                if(formato=="preFixa"):
-                    data=exp.preFixa()
-                    conn.sendall(data.encode()) #Enviar dado para o cliente
+                data2 = conn.recv(1024) # Segundo dado recebido, o método
+                metodo = data2.decode().upper()
+                if(metodo=="POSFIXA"):
+                    ExpConvertida=exp.posFixa()
+                    conn.sendall(ExpConvertida.encode()) # Envia dado Posfixa para o cliente
+                if(metodo=="PREFIXA"):
+                    ExpConvertida=exp.preFixa()
+                    conn.sendall(ExpConvertida.encode()) # Envia dado Prefixa para o cliente
+        else:
+            conn.close()
+except KeyboardInterrupt:
+    print(" Aplicação Servidor encerrada...")
+except:
+    print(" Problema na aplicação Servidor")
